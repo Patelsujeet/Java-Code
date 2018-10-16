51986e924e3683871c439f6e8d16c4f190201a8e
Patel Sujeet <PatelSujeet24051996@gmail.com>
2018-10-16 15:14:58 +0530

    Initial commit

---
+++ src/Classes/Classes.java
@@ -0,0 +1,8 @@
+public class Classes
+{
+	public static void main(String []arg)
+	{
+		System.out.println("This is ClassS");
+	}
+}
+
---
+++ src/Page/Page.java
@@ -0,0 +1,7 @@
+public class Page
+{
+	public static void main(String []arg)
+	{
+		System.out.println("This is page");
+	}
+}
